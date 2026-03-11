eaab3b4f8f42aca5ed580b119ccf5b4e154561f4
brunoscaratti99 <brunoscaratti99@gmail.com>
2026-03-11 12:21:53 -0300

    Add file format functions

--- src/Data/file_utils.py
+++ src/Data/file_utils.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def change_comma(frame):
   new_frame = frame.copy()
@@ -21,3 +22,25 @@ def format_path(path):
     return path
 
 
+
+def create_next_experiment_folder(base_path):
+    os.makedirs(base_path, exist_ok=True)
+    
+    existing = [
+        d for d in os.listdir(base_path)
+        if os.path.isdir(os.path.join(base_path, d)) and d.startswith("exp_")
+    ]
+    
+    numbers = []
+    for folder in existing:
+        match = re.match(r"exp_(\d+)", folder)
+        if match:
+            numbers.append(int(match.group(1)))
+            
+    next_number = max(numbers)+1 if numbers else 1
+    new_folder_name = f"exp_{next_number:03d}"
+    
+    full_path = os.path.join(base_path, new_folder_name)
+    os.makedirs(full_path)
+    
+    return full_path
