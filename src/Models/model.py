bb73dd60f60115adda9df52a75ff9b7fcd16f8ff
brunoscaratti99 <brunoscaratti99@gmail.com>
2026-03-11 23:35:18 -0300

    Fix bugs

--- src/Models/model.py
+++ src/Models/model.py
@@ -12,29 +12,18 @@ class GLSTMCellBatch(nn.Module):
         self.learn_adj = learn_adj
         
         
-        if edge_weight is None and learn_adj==True:
-            self.edge_weight = torch.ones(edge_index.shape[1])
-        
-        if edge_weight is None and learn_adj==False:
-            self.edge_weight = None
-            
+        if edge_weight is None:
+            self.edge_weight = None if not learn_adj else torch.ones(edge_index.shape[1], device=edge_index.device)
         else:
-            self.edge_weight = self.edge_weight
-            
+            self.edge_weight = edge_weight.to(edge_index.device) if torch.is_tensor(edge_weight) else torch.tensor(edge_weight, device=edge_index.device)
 
-
+        A_init = adjacency_matrix(N, edge_index, self.edge_weight)
         if learn_adj:
-            self.A = nn.Parameter(adjacency_matrix(N, edge_index, self.edge_weight))
+            self.A = nn.Parameter(A_init)
         else:
-            self.A = adjacency_matrix(N, edge_index, self.edge_weight)
+            self.register_buffer("A", A_init)
             
-        """
-        else:
-            self.gcn_h = GCNConv(hidden_size, hidden_size)
-            self.gcn_c = GCNConv(hidden_size, hidden_size)
-        """
-
-
+        
         self.W_i = nn.Linear(input_size, hidden_size).to(device)
         self.W_f = nn.Linear(input_size, hidden_size).to(device)
         self.W_o = nn.Linear(input_size, hidden_size).to(device)
@@ -47,14 +36,8 @@ class GLSTMCellBatch(nn.Module):
 
     def forward(self, X, H_prev, C_prev):
         # X: [B, N, F], H/C: [B, H, N]
+        T, N, F = X.shape
         if self.learn_adj:
-            
-            #A_eff = self.A
-            #self.A = nn.Parameter(self.A+self.A.T)
-            #A_raw = self.A
-            #A_pos = F.softplus(A_raw)
-            #A_eff = A_pos / (A_pos.sum(dim=1, keepdim=True)+1e-6)
-  
                     
             H_graph = torch.einsum("bhm,mn->bhn", H_prev, self.A).to(device)
             C_graph = torch.einsum("bhm,mn->bhn", C_prev, self.A).to(device)
@@ -63,10 +46,9 @@ class GLSTMCellBatch(nn.Module):
             Hg = H_graph.permute(0, 2, 1).to(device)
             Hp = H_prev.permute(0, 2, 1).to(device)
         else:
-            A_eff = self.A
-            
-            H_graph = torch.einsum("bhm,mn->bhn", H_prev, A_eff).to(device)
-            C_graph = torch.einsum("bhm,mn->bhn", C_prev, A_eff).to(device)
+
+            H_graph = torch.einsum("bhm,mn->bhn", H_prev, self.A).to(device)
+            C_graph = torch.einsum("bhm,mn->bhn", C_prev, self.A).to(device)
 
 
             Hg = H_graph.permute(0, 2, 1).to(device)
@@ -86,35 +68,49 @@ class GLSTMCellBatch(nn.Module):
 
 
 class GLSTMBatch(nn.Module):
-    def __init__(self, N, edge_index, in_channels, hidden_size, out_channels=1, learn_adj=True):
+    def __init__(self, N, edge_index, in_channels, hidden_size, out_channels, lstm_layers=1, learn_adj=True):
         super().__init__()
         self.N = N
+        self.lstm_layers = lstm_layers
         self.hidden_size = hidden_size
-        self.cell = GLSTMCellBatch(N, in_channels, hidden_size, edge_index, learn_adj=learn_adj)
+        self.window = out_channels
+        
+        #LSTM blocks
+        self.cell_0 = GLSTMCellBatch(N, in_channels, hidden_size, edge_index, learn_adj=learn_adj)
+        self.cells = nn.ModuleList([GLSTMCellBatch(N=N, input_size=hidden_size, hidden_size=hidden_size,edge_index=edge_index, learn_adj=learn_adj) for _ in range(self.lstm_layers-1)])
+        
+        #fully connected layer
         self.fc = nn.Sequential(
-            nn.Dropout(0.3),
-            nn.Linear(hidden_size, 1)
-            
-            #nn.Softplus(beta=0.5),
-            #nn.Linear(hidden_size, hidden_size),
-            #nn.Softplus(beta=0.5),
-            #nn.Linear(hidden_size, hidden_size//2),
-            #nn.Softplus(beta=0.5),
-            #nn.Linear(hidden_size//2, out_channels)
-            #nn.ReLU(),
-            #nn.Linear(hidden_size//4, out_channels)
-            #nn.Sigmoid()
+            nn.Dropout(0.2),
+            nn.Linear(hidden_size, out_channels)
         ).to(device)
+        
 
     def forward(self, x_seq):
-        #print(x_seq.shape)
         # x_seq: [B, T, N, F]
-        B, T, _, _ = x_seq.shape
+        
+        B, T, N, _ = x_seq.shape
+
         H = torch.zeros((B, self.hidden_size, self.N), device=x_seq.device)
         C = torch.zeros((B, self.hidden_size, self.N), device=x_seq.device)
-
+        
+        
+        H_layers = torch.zeros((B, T, self.N, self.hidden_size), device=x_seq.device, dtype=x_seq.dtype)
+        
+        
         for t in range(T):
-            C, H = self.cell(x_seq[:, t], H, C)
-            #print(C.norm(), H.norm())
-        out = H.permute(0, 2, 1).to(device)  # [B, N, H]
-        return self.fc(out).squeeze(-1)  # [B, N]
+            C, H = self.cell_0(x_seq[:,t], H, C)
+            H_layers[:,t] = H.permute(0,2,1)
+        
+
+        for j in range(self.lstm_layers-1):
+                H = torch.zeros((B, self.hidden_size, self.N), device=x_seq.device)
+                C = torch.zeros((B, self.hidden_size, self.N), device=x_seq.device)
+                for t in range(T):
+
+                    C, H = self.cells[j](H_layers[:,t], H, C)
+                    H_layers[:,t] = H.permute(0,2,1)
+
+        out = H_layers[:,-1].to(device)
+
+        return self.fc(out).permute(0, 2, 1)   # [B, OUT, N]
